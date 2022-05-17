1356edd8b9aa8b0491a9e2325206ea132bb64384
Squange <78355315+SquangeEj@users.noreply.github.com>
2022-05-18 00:03:46 +0100

    added player shooting

---
+++ Baz Final/Assets/CannonBall.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour
+{
+    [SerializeField]
+    private int damage;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            if (other.GetComponent<EnemyScript>())
+            {
+                other.GetComponent<EnemyScript>().takedamage(damage);
+                Debug.Log("Worked");
+            }
+        }
+        else
+        {
+            StartCoroutine(death());
+        }
+    }
+    
+
+    IEnumerator death()
+    {
+        yield return new WaitForSeconds(5f);
+
+        Destroy(gameObject);
+
+        yield return null;
+    }
+}
---
+++ Baz Final/Assets/cannonscript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannonscript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Bullet;
+    [SerializeField]
+    private float fire,firerate,range;
+    // Update is called once per frame
+    void Update()
+    {
+        fire -= 1 * Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space) && fire < 0)
+        {
+            GameObject bullet = Instantiate(Bullet, transform.position + new Vector3 (0,5,0), Quaternion.identity);
+            bullet.GetComponent<Rigidbody>().AddForce(transform.right * -range);
+            fire = firerate;
+        }
+    }
+}
