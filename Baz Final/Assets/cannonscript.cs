605b5c9616151ee2b206cf2c2c82a067c57f0a04
Glopex <62385400+Glopex@users.noreply.github.com>
2022-05-18 12:04:27 +0200

    checkpoints and sound

--- Baz Final/Assets/cannonscript.cs
+++ Baz Final/Assets/cannonscript.cs
@@ -8,12 +8,15 @@ public class cannonscript : MonoBehaviour
     private GameObject Bullet;
     [SerializeField]
     private float fire,firerate,range;
+    [SerializeField]
+    private AudioSource soundsource;
     // Update is called once per frame
     void Update()
     {
         fire -= 1 * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space) && fire < 0)
         {
+            soundsource.Play(); 
             GameObject bullet = Instantiate(Bullet, transform.position + new Vector3 (0,5,0), Quaternion.identity);
             bullet.GetComponent<Rigidbody>().AddForce(transform.right * -range);
             fire = firerate;
